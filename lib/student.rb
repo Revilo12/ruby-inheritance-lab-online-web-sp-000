74a4d6aa9e15445b840e8389b98118d406f360c4
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-12 15:03:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,12 @@
-class Student 
+class Student
   attr_reader :knowledge
 
-  def initialize 
-    @knowledge = [] 
+  def initialize
+    @knowledge = []
   end
 
   def learn(string)
-    @knowledge << string 
+    @knowledge << string
   end
 
 end
